3dc4fb0cd13b9f43d70c35042aae47c4f9c36a36
evan128 <156957654+evan128@users.noreply.github.com>
2026-02-16 01:52:08 +0000

    feat: Add function to parse metadata

--- vector_db.py
+++ vector_db.py
@@ -1,6 +1,6 @@
 import chromadb
 
-def md_to_string(file_path):
+def md_to_string(file_path: str) -> str:
     """
     Reads the content of a Markdown file into a string.
 
@@ -16,6 +16,37 @@ def md_to_string(file_path):
         return markdown_str
     except FileNotFoundError:
         return f"The file '{file_path}' was not found."
+    
+def parse_metadata(md_text: str):
+    """
+    Get document metatdata and remaining text so it can be parsed
+
+    Args:
+        file_path (str): The text of a Markdown document.
+
+    Returns:
+        str, str: The metadata as a string
+        and the remaining text in the document
+    """
+    _, metadata, remaining_text = md_text.split('---', 2)
+    return metadata, remaining_text.strip()
+    
+
+def chunk_by_courses(document_text: str) -> list[str]:
+    """
+    Breaks a document with courses into a list with courses
+    and their descriptions for every course in the document text.
+
+    Args:
+        document_text (str): Text of a document with courses.
+
+    Returns:
+        list[str]: A list of strings including
+        the course name and course information for every course.
+
+    """
+
+    
 
 # Link to Chroma docs gettng started tutorial
 # https://docs.trychroma.com/docs/overview/getting-started
@@ -27,6 +58,8 @@ chroma_client = chromadb.Client()
 collection = chroma_client.create_collection(name="university_documents")
 
 cs_courses_str = md_to_string('/workspaces/university-knowledge-assistant/output/Courses • Southwestern University_extracted.md')
+
+
 
 # Chroma stores text and handles embedding and indexing automatically
 collection.add(
