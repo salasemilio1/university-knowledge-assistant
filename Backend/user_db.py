32f2341436d669695fb44a880b158ed091c494cc
Frank Desilets <desiletsfrank4@gmail.com>
2026-03-29 17:26:54 +0000

    document user model and implement create_db_and_tables() and get_async_session in user_db.py (#49)

--- Backend/user_db.py
+++ Backend/user_db.py
@@ -10,4 +10,26 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
+    """
+    Used as the base class for all SQLAlchemy models.
+    """
     pass
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def create_db_and_tables():
+    """
+    Creates the db tables from each existing model.
+    """
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
