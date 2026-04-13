ed42fdc99ef853f78e1797d4ac20b6615742a5ad
Alyanna Martinez <151060410+martineza088@users.noreply.github.com>
2026-04-13 04:13:11 +0000

    Separated course name and course code into seperate patterns to consolidate course info

--- scripts/transc-ingestion.py
+++ scripts/transc-ingestion.py
@@ -1,4 +1,4 @@
-from pypdf import PdfReader, PdfWriter
+import pdfplumber
 import re
 import os 
 from dotenv import load_dotenv
@@ -6,33 +6,48 @@ from dotenv import load_dotenv
 load_dotenv()
 FILE_PATH = os.getenv("TRANSC_PATH")
 
-reader = PdfReader(FILE_PATH)
+text = ""
+with pdfplumber.open(FILE_PATH) as pdf:
+    page = pdf.pages[0]
 
-writer = PdfWriter()
+    w = page.width
+    h = page.height
+    print(f"width: {w} and height: {h}")
+    bounding_box = (0, 167, 612, 792)
+    cropped_page = page.within_bbox(bounding_box)
 
-for page in reader.pages:
-    # Set the crop box using coordinates (lower_left_x, lower_left_y, upper_right_x, upper_right_y)
-    page.mediabox.lower_left = (0, 0)
-    page.mediabox.upper_right = (500, 625)  # 800, 925
-    writer.add_page(page)
+    text = cropped_page.extract_text()
+    print(text, "test")
 
-with open("cropped_transc.pdf", "wb") as fp:
-    writer.write(fp)
+#course_pattern = r"\b([A-Z]{2,4}\d{2}-\d{3})\b[A-Z]{3}\d{2}-\d{3}\s+(.+?)\s+\d+\.\d{2}\s+[A-F][+-]?\s+\d+\.\d{2}\b"
+course_code_pattern = r"\b[A-Z]{3}\d{2}-\d{3}\b"
+course_name_pattern = r"\b[A-Z]{3}\d{2}-\d{3}\s+(.+?)\s+\d+\.\d{2}\s+[A-F][+-]?\s+\d+\.\d{2}\b"
+course_credits_and_grade_pattern = r"\b(\d+\.\d{2})\s+([A-F][+-]?)\b" # can be broken up in groups
 
-reader_cropped = PdfReader("cropped_transc.pdf")
-# Extract text from the first page
-page = reader_cropped.pages[0]
-text = page.extract_text()
-print(text)
 
-# some regex for pattern-matching in the transcript
-course_pattern = r"\b[A-Z]{3}\d{2}-\d{3}\b"
+# course_list = re.findall(course_pattern, text)
+course_code_list = re.findall(course_code_pattern, text)
+course_name_list = re.findall(course_name_pattern, text)
+match = re.search(course_credits_and_grade_pattern, text)
+course_credits, course_grade = match.groups()
 
-text2 = """
-MAT52-164 Modern Calculus I
-ENS78-101 SWE
-"""
+print(f"Course Codes: {course_code_list}")
+print(f"\nNumber of Course Codes: {len(course_code_list)}")
 
-matches = re.findall(course_pattern, text2)
+print(f"\nCourse Names: {course_name_list}")
+print(f"\nNumber of Course Names: {len(course_name_list)}")
+# print(f"\nFull Course Information: {course_list}")
 
-print(matches)
+print(f"\nCourse Credits + Grade Pattern: {course_credits_and_grade_pattern}")
+print(f"\nCourse Credits and Grade: {course_credits} and {course_grade}")
+
+
+#pattern = r"\b([A-Z]{3}\d[A-Z]-\d{3})\s+(.+?)\s+(\d+\.\d{2})\s+([A-F][+-]?)\s+(\d+\.\d{2})\b"
+pattern = r"\b([A-Z]{2,4}\d{2}-\d{3})\s+(.+?)\s+(\d+\.\d{2})\s+([A-F][+-]?)\s+(\d+\.\d{2})\b"
+
+match = re.search(pattern, text)
+print(f"Matches: {match}")
+if match:
+    course_code, name, credits, grade, points = match.groups()
+    print(course_code, name, credits, grade, points)
+    print("testing")
